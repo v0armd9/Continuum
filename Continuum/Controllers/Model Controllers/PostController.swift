af490ca9a5fb8766e57c66f9da5d973921b86ccb
Darin Armstrong <darin.marmstrong@gmail.com>
2019-07-09 15:49:06 -0600

    adds CRUD functions in PostController -created PostTableViewCell file, but no code has been added to this file

---
+++ Continuum/Controllers/Model Controllers/PostController.swift
@@ -0,0 +1,33 @@
+//
+//  PostController.swift
+//  Continuum
+//
+//  Created by Darin Marcus Armstrong on 7/9/19.
+//  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class PostController {
+    
+    //Shared Instance (singleton)
+    static let sharedInstance = PostController()
+
+    //Source of Truth
+    var posts: [Post] = []
+    
+    //Creates a function that takes in text(String) and a post(Post)
+    //--along with a closure that takes in a comment and returns void
+    func addComment(with text: String, post: Post, completion: @escaping(Comment) -> Void) {
+        let newComment = Comment(text: text, post: post)
+        post.comments.append(newComment)
+    }
+    
+    func createPostWith(image: UIImage, caption: String, completion: @escaping(Post?) -> Void) {
+        let newPost = Post(photo: image, caption: caption)
+        self.posts.append(newPost)
+    }
+    
+    
+}//End of Class
+
---
+++ Continuum/Views/Custom Views/PostTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostTableViewCell.swift
+//  Continuum
+//
+//  Created by Darin Marcus Armstrong on 7/9/19.
+//  Copyright © 2019 Darin Marcus Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class PostTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
